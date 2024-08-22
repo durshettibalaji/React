b9e1b3ef0144fc08409614380a6be2c6f9364588
DAI\durshetti.balaji <durshetti.balaji@williamoneilindia.com>
2024-08-22 10:28:18 +0530

    inception

---
+++ app.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1",{id:"heading"},"hello world");  // in object we give attributes
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(heading);
