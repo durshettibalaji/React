1bad807b6ad49c953363ff46a480641e42e7bd46
DAI\durshetti.balaji <durshetti.balaji@williamoneilindia.com>
2024-10-17 17:56:57 +0530

    episode 1

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-const heading = React.createElement("h1",{id:"heading"},"hello world");  // in object we give attributes
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
+const heading = React.createElement("h1", { id: "heading" }, "hello world");  // in object we give attributes
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+console.log(heading);
+console.log(root);
+
+const nested = React.createElement("div",{id: "parent"},React.createElement("div",{id: "child"},React.createElement("h1",{id: "head"},"header")));
+const nest = ReactDOM.createRoot(document.getElementById("nest"));
+nest.render(nested);
+
+
+const siblings = React.createElement("div",{id: "parent"},React.createElement("div",{id: "child"},[React.createElement("h1",{id: "head"},"child 1"),React.createElement("h1",{id: "head"},"child 2")]));
+const sibling = ReactDOM.createRoot(document.getElementById("sibling"));
+sibling.render(siblings);
