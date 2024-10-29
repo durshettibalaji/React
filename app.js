8b016b50fffd5e4dc3fb6e37ae62a72546fa3205
DAI\durshetti.balaji <durshetti.balaji@williamoneilindia.com>
2024-10-29 18:30:35 +0530

    episode 2 bundlers

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
+import React from "react";
+import { createRoot,ReactDOM } from "react-dom/client";
 const heading = React.createElement("h1", { id: "heading" }, "hello world");  // in object we give attributes
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(heading);
 console.log(heading);
 console.log(root);
 
-const nested = React.createElement("div",{id: "parent"},React.createElement("div",{id: "child"},React.createElement("h1",{id: "head"},"header")));
-const nest = ReactDOM.createRoot(document.getElementById("nest"));
+const nested = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, React.createElement("h1", { id: "head" }, "header")));
+const nest = createRoot(document.getElementById("nest"));
 nest.render(nested);
 
 
-const siblings = React.createElement("div",{id: "parent"},React.createElement("div",{id: "child"},[React.createElement("h1",{id: "head"},"child 1"),React.createElement("h1",{id: "head"},"child 2")]));
-const sibling = ReactDOM.createRoot(document.getElementById("sibling"));
+const siblings = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, [React.createElement("h1", { id: "head" }, "child 1"), React.createElement("h1", { id: "head" }, "child 2")]));
+const sibling = createRoot(document.getElementById("sibling"));
 sibling.render(siblings);
