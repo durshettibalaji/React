a485d8b8632746e4c3e8105493b8322c92e7f54d
durshettibalaji <durshettibalaji1@gmail.com>
2024-10-30 12:58:19 +0530

    episode 3

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 import React from "react";
 import { createRoot, ReactDOM } from "react-dom/client";
 const jsxHeader = <h1>jsx is html like syntax used to createreact elements </h1>
+const num=1234;
+const ReactComponent = () =>{
+    return ( <div><InnerComponent/><h1> i am an react component {num*5}  i will retun the jsx</h1></div> )
+}
+const InnerComponent = () =>(<h1>I am inner component</h1>)
 const root = createRoot(document.getElementById("root"));
-root.render(jsxHeader); 
+root.render(<ReactComponent/>); 
